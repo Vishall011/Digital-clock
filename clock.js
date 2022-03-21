b022d6cdc5579bf65686b8ac9696dc45d6472172
Vishall011 <88180180+Vishall011@users.noreply.github.com>
2022-03-21 18:17:17 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,19 @@
+setInterval(() => {
+    const time = document.querySelector("#time");
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = 'AM';
+
+    if (hh > 12)
+        hh = hh - 12;
+    session = 'PM';
+    
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
+
+    time.textContent = hh + ":" + mm + ":" + ss + " " + session;
+
+});
